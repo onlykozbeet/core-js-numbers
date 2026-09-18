6844a388d20402ba6fd1344fc8aea741f3d8d5b1
Konstantin <kozbeet@proton.me>
2026-09-18 11:36:31 +0300

    add solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -67,8 +67,10 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const pointX = (x2 - x1) ** 2;
+  const pointY = (y2 - y1) ** 2;
+  return Math.sqrt(pointX + pointY);
 }
 
 /**
@@ -83,8 +85,8 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 /**
@@ -104,8 +106,11 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const magn1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const magn2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  return Math.acos(dotProduct / (magn1 * magn2));
 }
 
 /**
